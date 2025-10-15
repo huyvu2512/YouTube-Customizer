7afd5cd89e890c4de089caa2256b6909d6a0f0d4
Huy Vũ <quanghuy25121995@gmail.com>
2025-10-16 00:48:21 +0700

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         YouTube: Logo Premium & 4 Cột Video
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Thay đổi logo YouTube thành Premium và tùy chỉnh số cột video thành 4.
+// @author       Huy Vũ
+// @match        https://www.youtube.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
