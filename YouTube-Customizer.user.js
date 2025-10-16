addc42ff437892342d229102efe1f360d674a0cc
Huy Vũ <quanghuy25121995@gmail.com>
2025-10-16 17:29:57 +0700

    Update URLs and maintain script metadata

--- YouTube-Customizer.user.js
+++ YouTube-Customizer.user.js
@@ -5,12 +5,13 @@
 // @description  Thay đổi logo YouTube thành Premium và tùy chỉnh số cột video thành 4.
 // @author       Huy Vũ
 // @match        https://www.youtube.com/*
-// @icon         https://raw.githubusercontent.com/huyvu2512/YouTube-Logo-Premium-4-C-t-Video/main/icon.png
-// @require      https://raw.githubusercontent.com/huyvu2512/YouTube-Logo-Premium-4-C-t-Video/main/youtube_customizer.js
-// @updateURL    https://raw.githubusercontent.com/huyvu2512/YouTube-Logo-Premium-4-C-t-Video/main/YouTube-Customizer.user.js
-// @downloadURL  https://raw.githubusercontent.com/huyvu2512/YouTube-Logo-Premium-4-C-t-Video/main/YouTube-Customizer.user.js
+// @icon         https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/icon.png
+// @require      https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/YouTube-Customizer.user.js
+// @updateURL    https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/YouTube-Customizer.user.js
+// @downloadURL  https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/YouTube-Customizer.user.js
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
 // Mã được tải tự động từ @require. Không cần thêm gì ở đây.
+
