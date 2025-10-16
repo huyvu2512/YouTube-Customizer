9402bbd1e896d0b1653fe85427fd2cfab1bd7733
Huy Vũ <quanghuy25121995@gmail.com>
2025-10-16 17:23:54 +0700

    Update and rename youtube_customizer.js to YouTube-Customizer.user.js

---
+++ YouTube-Customizer.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         YouTube: Logo Premium & 4 Cột Video
+// @namespace    http://tampermonkey.net/
+// @version      2.1
+// @description  Thay đổi logo YouTube thành Premium và tùy chỉnh số cột video thành 4.
+// @author       Huy Vũ
+// @match        https://www.youtube.com/*
+// @icon         https://raw.githubusercontent.com/huyvu2512/YouTube-Logo-Premium-4-C-t-Video/main/icon.png
+// @require      https://raw.githubusercontent.com/huyvu2512/YouTube-Logo-Premium-4-C-t-Video/main/youtube_customizer.js
+// @updateURL    https://raw.githubusercontent.com/huyvu2512/YouTube-Logo-Premium-4-C-t-Video/main/YouTube-Customizer.user.js
+// @downloadURL  https://raw.githubusercontent.com/huyvu2512/YouTube-Logo-Premium-4-C-t-Video/main/YouTube-Customizer.user.js
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+// Mã được tải tự động từ @require. Không cần thêm gì ở đây.
