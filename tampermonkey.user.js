533b3692265fafd71542d0ab5867d427b782cd22
Huy Vũ <quanghuy25121995@gmail.com>
2025-10-16 17:32:15 +0700

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -4,14 +4,9 @@
 // @version      1.0
 // @description  Thay đổi logo YouTube thành Premium và tùy chỉnh số cột video thành 4.
 // @author       Huy Vũ
+// @require      https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/tampermonkey.user.js
 // @match        https://www.youtube.com/*
-// @icon         https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/icon.png
-// @require      https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/YouTube-Customizer.user.js
-// @updateURL    https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/YouTube-Customizer.user.js
-// @downloadURL  https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/YouTube-Customizer.user.js
 // @run-at       document-start
 // @grant        none
+// @icon         https://raw.githubusercontent.com/huyvu2512/YouTube-Customizer/main/icon.png
 // ==/UserScript==
-
-// Mã được tải tự động từ @require. Không cần thêm gì ở đây.
-
